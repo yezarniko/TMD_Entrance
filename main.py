59bc8992ba4ea147cdb705740e90f2882ef412ea
yezarniko <yezarniko@protonmail.com>
2024-01-17 12:49:04 +0630

    complete template

--- main.py
+++ main.py
@@ -4,21 +4,27 @@ app = Flask("TMD_Entrance")
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('first.html')
 
 
-@app.route('/grade11/', methods=['GET', 'POST'])
-def grade11():
+@app.route('/entrance')
+def entrance():
+    return render_template('entrance.html')
+
+
+@app.route('/grade10test/', methods=['GET', 'POST'])
+def grade10_test():
     if request.method == 'GET':
-        return render_template('grade11.html')
+        return render_template('g10test.html')
     elif request.method == 'POST':
-        pass
+        print(request.form.get('color'))
+        return render_template('g10test.html')
 
 
-@app.route('/grade12', methods=['GET', 'POST'])
-def grade12():
+@app.route('/grade11test/', methods=['GET', 'POST'])
+def grade11_test():
     if request.method == 'GET':
-        return render_template('grade12.html')
+        return render_template('g11test.html')
     elif request.method == 'POST':
         pass
 
